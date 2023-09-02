e449d29f6889615c312e9f1171305a64e09e4562
akif <70863898+akif-ned@users.noreply.github.com>
2023-09-02 10:23:21 +0200

    firstJavascript

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,4 +1,5 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const assert = require ('assert');
 
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').build();
@@ -7,7 +8,21 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
     await driver.findElement(By.id("CybotCookiebotDialogBodyButtonDecline")).click();
 
+    await driver.sleep(2000);
+
     await driver.findElement(By.css(".shared-header__nav > form[role='search']  input#shared-header-search")).sendKeys("med",Key.ENTER);
+
+    //assert
+    let searchResult= await driver.findElement(By.css("h1"))
+
+    console.log(searchResult.getText());
+    
+
+    await driver.sleep(2000);
+
+    let srcBxIsDisplayed=searchResult.isDisplayed;
+
+    assert.strictEqual(searchResult.getText(),"Zoekresultaten");
 
     await driver.sleep(5000);
     
